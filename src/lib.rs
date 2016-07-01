e770ebb5eb62d6a8ad5ef166740c45ee20376d2e
Martin Evans <martindevans@gmail.com>
2016-07-01 16:08:37 +0100

    Simplified names of nouns (imported them, instead of prefixing the namespace every time)
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,19 @@ enum Value
     Cell(Rc<Value>, Rc<Value>)
 }
 
+use self::Value::{ Error, Atom, Cell };
+
 impl Value {
     fn atom_value(&self) -> Option<i32> {
         match self {
-            &Value::Atom(a) => Some(a),
+            &Atom(a) => Some(a),
             _ => None
         }
     }
 
     fn cell_content(&self) -> Option<(Rc<Value>, Rc<Value>)> {
         match self {
-            &Value::Cell(ref a, ref b) => Some((a.clone(), b.clone())),
+            &Cell(ref a, ref b) => Some((a.clone(), b.clone())),
             _ => None
         }
     }
@@ -26,66 +28,66 @@ impl Value {
 
 fn kind(v: &Value) -> Value {
     match v {
-        &Value::Error => Value::Error,
-        &Value::Atom(_) => Value::Atom(0),
-        &Value::Cell(_, _) => Value::Atom(1),
+        &Error => Error,
+        &Atom(_) => Atom(0),
+        &Cell(_, _) => Atom(1),
     }
 }
 
 fn sub(v: &Value) -> Value {
     match v {
-        &Value::Error => Value::Error,
+        &Error => Error,
 
         // math- a
-        &Value::Atom(v) => Value::Atom(-v),
+        &Atom(v) => Atom(-v),
 
         // math- [a, b]
         // math- [[a, b], c]
         // math- [a, [b, c]]
         // math- [[a, b], [c, d]]
-        &Value::Cell(ref a, ref b) => sub_cell(&a, &b)
+        &Cell(ref a, ref b) => sub_cell(&a, &b)
     }
 }
 
-// Equivalent to calling sub(Value::Cell(a, b))
+// Equivalent to calling sub(Cell(a, b))
 fn sub_cell(a: &Value, b: &Value) -> Value {
     match (a, b) {
         // math- [a, b]
-        (&Value::Atom(a), &Value::Atom(b)) => Value::Atom(a - b),
+        (&Atom(a), &Atom(b)) => Atom(a - b),
 
         // math- [[a, b], c] => [math- [a, c], math- [b, c]]
-        (&Value::Cell(ref a, ref b), &Value::Atom(c)) => Value::Cell(
-            Rc::new(sub_cell(&a, &Value::Atom(c))),
-            Rc::new(sub_cell(&b, &Value::Atom(c)))
+        (&Cell(ref a, ref b), &Atom(c)) => Cell(
+            Rc::new(sub_cell(&a, &Atom(c))),
+            Rc::new(sub_cell(&b, &Atom(c)))
         ),
 
         // math- [a, [b, c]] => [math- [a, b], math- [a, c]]
-        (&Value::Atom(a), &Value::Cell(ref b, ref c)) => Value::Cell(
-            Rc::new(sub_cell(&Value::Atom(a), &b)),
-            Rc::new(sub_cell(&Value::Atom(a), &c))
+        (&Atom(a), &Cell(ref b, ref c)) => Cell(
+            Rc::new(sub_cell(&Atom(a), &b)),
+            Rc::new(sub_cell(&Atom(a), &c))
         ),
 
         // math- [[a, b], [c, d]] => [math- [a, c], math- [b, d]]
-        (&Value::Cell(ref a, ref b), &Value::Cell(ref c, ref d)) => Value::Cell(
+        (&Cell(ref a, ref b), &Cell(ref c, ref d)) => Cell(
             Rc::new(sub_cell(&a, &c)),
             Rc::new(sub_cell(&b, &d)),
         ),
 
-        _ => Value::Error
+        _ => Error
     }
 }
 
 fn eq(v: &Value) -> Value {
     match v {
-        &Value::Error => Value::Error,
+        &Error => Error,
 
         // math= a => Error
-        &Value::Atom(v) => Value::Error,
+        &Atom(v) => Error,
 
         // math= [a, a] => 1
         // math= [a, b] => 0
         // math= [[a, b], [c, d]] => math= [math= [a, c], math= [b, d]]
-        &Value::Cell(ref a, ref b) => eq_cell(&a, &b)
+        &Cell(ref a, ref b) => eq_cell(&a, &b)
     }
 }
 
@@ -94,47 +96,66 @@ fn eq_cell(a: &Value, b: &Value) -> Value {
 
         // math= [a, a] => 1
         // math= [a, b] => 0
-        (&Value::Atom(a), &Value::Atom(b)) => Value::Atom(if a == b { 1 } else { 0 }),
+        (&Atom(a), &Atom(b)) => Atom(if a == b { 1 } else { 0 }),
 
         // math= [[a, b], [c, d]] => math= [math= [a, c], math= [b, d]]
-        (&Value::Cell(ref a, ref b), &Value::Cell(ref c, ref d)) => eq_cell(
+        (&Cell(ref a, ref b), &Cell(ref c, ref d)) => eq_cell(
             &eq_cell(a, c),
             &eq_cell(b, d)
         ),
 
         //If we didn't find a structure for subtree equality this is just a case of `math=[a, b] => 0`
-        _ => Value::Atom(0)
+        _ => Atom(0)
     }
 }
 
 fn swap(v: &Value) -> Value {
     match v {
-        &Value::Error => Value::Error,
-        &Value::Atom(a) => Value::Atom(a),
-        &Value::Cell(ref a, ref b) => Value::Cell(b.clone(), a.clone())
+        &Error => Error,
+        &Atom(a) => Atom(a),
+        &Cell(ref a, ref b) => Cell(b.clone(), a.clone())
     }
 }
 
-fn eval(v: &Value) -> Value {
+fn eval(v: &Value) -> Rc<Value> {
     match v {
-        &Value::Error => Value::Error,
-        &Value::Atom(a) => Value::Error,
+        &Error => Rc::new(Error),
+        &Atom(a) => Rc::new(Error),
+        &Cell(ref a, ref b) => eval_cell(&a, &b)
+    }
+}
 
-        &Value::Cell(ref a, ref b) => {
-            match a.atom_value() {
-                Some(a) => {
-                    match (a, b) {
-                        (0, _) => kind(&b),
-                        (1, _) => sub(&b),
-                        (2, _) => eq(&b),
-                        (3, _) => swap(&b),
+fn eval_cell(a: &Value, b: &Value) -> Rc<Value> {
+    match (a.atom_value(), b) {
+        (None, _) => Rc::new(Error),
+        (Some(0), _) => Rc::new(kind(&b)),
+        (Some(1), _) => Rc::new(sub(&b)),
+        (Some(2), _) => Rc::new(eq(&b)),
+        (Some(3), _) => Rc::new(swap(&b)),
 
-                        _ => Value::Error
-                    }
-                },
-                _ => Value::Error
+        // eval! [4, [a, [b, c]] => eval! [ eval! [a, b], eval! [a, c] ]
+        (Some(4), &Cell(ref a, ref bc)) => {
+            match **bc {
+                Error => Rc::new(Error),
+                Atom(_) => Rc::new(Error),
+                Cell(ref b, ref c) => Rc::new(eval_cell(
+                    &eval_cell(&a, &b),
+                    &eval_cell(&a, &c)
+                ))
+            }
+        },
+
+        // eval! [5, [0, [b, c]]] => b
+        // eval! [5, [1, [b, c]]] => c
+        (Some(5), &Cell(ref a, ref bc)) => {
+            match (a.atom_value(), bc.cell_content()) {
+                (Some(0), Some((ref b, ref c))) => Rc::new(b),
+                (Some(1), Some((ref b, ref c))) => Rc::new(c),
+                _ => Rc::new(Error)
             }
         }
+
+        _ => Rc::new(Error)
     }
 }
 
@@ -147,13 +168,13 @@ mod tests {
     #[test]
     fn kind_of_error_is_error() {
         // eval! [0, Error] => kind? Error => Error
-        let r = eval(&Value::Cell(
-            Rc::new(Value::Atom(0)),
-            Rc::new(Value::Error)
+        let r = eval(&Cell(
+            Rc::new(Atom(0)),
+            Rc::new(Error)
         ));
 
         match r {
-            Value::Error => assert!(true),
+            Error => assert!(true),
             _ => assert!(false)
         };
     }
@@ -161,16 +182,16 @@ mod tests {
     #[test]
     fn kind_of_cell_is_cell() {
         // eval! [0, Error] => kind? [1, 2] => 1
-        let r = eval(&Value::Cell(
-            Rc::new(Value::Atom(0)),
-            Rc::new(Value::Cell(
-                Rc::new(Value::Atom(1)),
-                Rc::new(Value::Atom(2)),
+        let r = eval(&Cell(
+            Rc::new(Atom(0)),
+            Rc::new(Cell(
+                Rc::new(Atom(1)),
+                Rc::new(Atom(2)),
             ))
         ));
 
         match r {
-            Value::Atom(1) => assert!(true),
+            Atom(1) => assert!(true),
             _ => assert!(false)
         };
     }
@@ -178,62 +199,62 @@ mod tests {
     #[test]
     fn kind_of_atom_is_atom() {
         // eval! [0, 1] => kind? 1 => 0
-        let r = eval(&Value::Cell(
-            Rc::new(Value::Atom(0)),
-            Rc::new(Value::Atom(1))
+        let r = eval(&Cell(
+            Rc::new(Atom(0)),
+            Rc::new(Atom(1))
         ));
 
         match r {
-            Value::Atom(0) => assert!(true),
+            Atom(0) => assert!(true),
             _ => assert!(false)
         };
     }
 
     #[test]
     fn math_sub_atom_is_negate() {
-        let r = sub(&Value::Atom(-1));
+        let r = sub(&Atom(-1));
 
         match r {
-            Value::Atom(1) => assert!(true),
+            Atom(1) => assert!(true),
             _ => assert!(false)
         }
     }
 
     #[test]
     fn math_sub_error_is_error() {
-        let r = sub(&Value::Error);
+        let r = sub(&Error);
 
         match r {
-            Value::Error => assert!(true),
+            Error => assert!(true),
             _ => assert!(false)
         }
     }
 
     #[test]
     fn math_sub_cell_is_sub() {
-        let r = sub(&Value::Cell(
-            Rc::new(Value::Atom(1)),
-            Rc::new(Value::Atom(2)),
+        let r = sub(&Cell(
+            Rc::new(Atom(1)),
+            Rc::new(Atom(2)),
         ));
 
         match r {
-            Value::Atom(-1) => assert!(true),
+            Atom(-1) => assert!(true),
             _ => assert!(false)
         }
     }
 
     #[test]
     fn math_sub_atom_from_cell() {
-        let r = sub(&Value::Cell(
-            Rc::new(Value::Cell(
-                Rc::new(Value::Atom(1)),
-                Rc::new(Value::Atom(2))
+        let r = sub(&Cell(
+            Rc::new(Cell(
+                Rc::new(Atom(1)),
+                Rc::new(Atom(2))
             )),
-            Rc::new(Value::Atom(3))
+            Rc::new(Atom(3))
         ));
 
         match r {
-            Value::Cell(a, b) => {
+            Cell(a, b) => {
                 match (a.atom_value(), b.atom_value()) {
                     (Some(-2), Some(-1)) => assert!(true),
                     _ => assert!(false)
@@ -245,16 +266,16 @@ mod tests {
 
     #[test]
     fn math_sub_cell_from_atom() {
-        let r = sub(&Value::Cell(
-            Rc::new(Value::Atom(3)),
-            Rc::new(Value::Cell(
-                Rc::new(Value::Atom(1)),
-                Rc::new(Value::Atom(2))
+        let r = sub(&Cell(
+            Rc::new(Atom(3)),
+            Rc::new(Cell(
+                Rc::new(Atom(1)),
+                Rc::new(Atom(2))
             )),
         ));
 
         match r {
-            Value::Cell(a, b) => {
+            Cell(a, b) => {
                 match (a.atom_value(), b.atom_value()) {
                     (Some(2), Some(1)) => assert!(true),
                     _ => assert!(false)
@@ -266,21 +287,21 @@ mod tests {
 
     #[test]
     fn math_sub_cell_from_cell() {
-        let r = sub(&Value::Cell(
-            Rc::new(Value::Cell(
-                Rc::new(Value::Atom(1)),
-                Rc::new(Value::Atom(2))
+        let r = sub(&Cell(
+            Rc::new(Cell(
+                Rc::new(Atom(1)),
+                Rc::new(Atom(2))
             )),
-            Rc::new(Value::Cell(
-                Rc::new(Value::Atom(3)),
-                Rc::new(Value::Atom(4))
+            Rc::new(Cell(
+                Rc::new(Atom(3)),
+                Rc::new(Atom(4))
             )),
         ));
 
         println!("{:?}", r);
 
         match r {
-            Value::Cell(a, b) => {
+            Cell(a, b) => {
                 match (a.atom_value(), b.atom_value()) {
                     (Some(-2), Some(-2)) => assert!(true),
                     _ => assert!(false)
@@ -292,58 +313,58 @@ mod tests {
 
     #[test]
     fn eq_error_is_error() {
-        match eq(&Value::Error) {
-            Value::Error => assert!(true),
+        match eq(&Error) {
+            Error => assert!(true),
             _ => assert!(false)
         }
     }
 
     #[test]
     fn eq_atom_is_error() {
-        match eq(&Value::Atom(1)) {
-            Value::Error => assert!(true),
+        match eq(&Atom(1)) {
+            Error => assert!(true),
             _ => assert!(false)
         }
     }
 
     #[test]
     fn eq_cell_is_equal_with_equal_atoms() {
-        match eq(&Value::Cell(Rc::new(Value::Atom(1)), Rc::new(Value::Atom(1)))) {
-            Value::Atom(1) => assert!(true),
+        match eq(&Cell(Rc::new(Atom(1)), Rc::new(Atom(1)))) {
+            Atom(1) => assert!(true),
             _ => assert!(false)
         }
     }
 
     #[test]
     fn eq_cell_is_unequal_with_unequal_atoms() {
-        match eq(&Value::Cell(Rc::new(Value::Atom(1)), Rc::new(Value::Atom(2)))) {
-            Value::Atom(0) => assert!(true),
+        match eq(&Cell(Rc::new(Atom(1)), Rc::new(Atom(2)))) {
+            Atom(0) => assert!(true),
             _ => assert!(false)
         }
     }
 
     #[test]
     fn eq_cell_is_equal_with_equal_subtrees() {
-        match eq(&Value::Cell(
-            Rc::new(Value::Cell(
-                Rc::new(Value::Atom(1)),
-                Rc::new(Value::Atom(2)),
+        match eq(&Cell(
+            Rc::new(Cell(
+                Rc::new(Atom(1)),
+                Rc::new(Atom(2)),
             )),
-            Rc::new(Value::Cell(
-                Rc::new(Value::Atom(1)),
-                Rc::new(Value::Atom(2)),
+            Rc::new(Cell(
+                Rc::new(Atom(1)),
+                Rc::new(Atom(2)),
             ))
         )) {
-            Value::Atom(1) => assert!(true),
+            Atom(1) => assert!(true),
             _ => assert!(false)
         }
     }
 
     #[test]
     fn swap_cell_swaps_sides() {
-        let v = swap(&Value::Cell(Rc::new(Value::Atom(1)), Rc::new(Value::Atom(2))));
+        let v = swap(&Cell(Rc::new(Atom(1)), Rc::new(Atom(2))));
         match v {
-            Value::Cell(ref a, ref b) => {
+            Cell(ref a, ref b) => {
                 match (a.atom_value(), b.atom_value()) {
                     (Some(2), Some(1)) => assert!(true),
                     _ => assert!(false)
